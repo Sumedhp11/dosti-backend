ec9c5ce5368492ddae5401c56fe6e72ab56e36a4
Sumedh Pawar <sumedhpawar8966@gmail.com>
2024-06-06 18:42:26 +0530

    added cors to socket

--- dist/app.js
+++ dist/app.js
@@ -29,7 +29,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173"],
+        origin: ["http://localhost:5173", "https://dosti-frontend.vercel.app"],
         credentials: true,
     },
 });
